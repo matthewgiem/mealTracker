eb1d25dc3a5b1bd7fb2ce2b663d9390cb6c4b13b
Matthew Giem <matthew.giem@gmail.com>
2016-08-14 18:20:32 -0700

    fix the display problem

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -1,12 +1,12 @@
 import { Component } from 'angular2/core';
-import { AppComponent } from './app.component';
-import { Meal } from './meal.model';
-import { SortPipe } from './sort.pipe';
 import { MealComponent } from './meal.component';
+import { Meal } from './meal.model';
+import { AppComponent } from './app.component';
+import { SortPipe } from './sort.pipe';
 
 @Component({
-  selector: "meal-list",
-  inputs: ["mealList"],
+  selector: 'meal-list',
+  inputs: ['mealList'],
   pipes: [SortPipe],
   directives: [MealComponent],
   templateUrl: 'app/meal-list.component.html'
--- app/meal.component.ts
+++ app/meal.component.ts
@@ -1,20 +1,16 @@
 import { Component } from 'angular2/core';
 import { Meal } from './meal.model';
-import {  } from './'
 
 @Component({
   selector: 'meal-display',
   inputs: ['meal'],
   template: `
-    <div>
-      <label>
-        <hr> what {{ meal.calories}}
-      </label>
-    </div>
+      <h2>
+        Name: {{ meal.food }}<br>Details: {{ meal.details }}<br>Calories: {{ meal.calories }}
+      </h2>
+      <hr>
   `
 })
 export class MealComponent  {
-  public meal: Meal[];
+  public meal: Meal;
 }
-
-      // name: {{ meal.food }}<br>Details: {{ meal.details }}<br>Calories: {{ meal.calories }}
